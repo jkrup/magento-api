cb2ee336f233bb7c39b750d5eb0faf92b46cfa6e
Ian Graham <imartingraham@gmail.com>
2015-06-12 08:55:51 -0700

    ICEUX-119 Added affirm payment endpoints

--- src/resources/checkout_cart_payment.js
+++ src/resources/checkout_cart_payment.js
@@ -42,7 +42,7 @@ var protos = {
         return data;
       } */
     }
-  },
+  }  },
   /**
    * Place order for paypal express payment
    */
@@ -56,6 +56,12 @@ var protos = {
    */
   paypalIpn: {
       mandatory: 'ipnData'
+  },
+  affirmPaymentsPlaceOrder:{
+    mandatory: 'store,quoteId,sessionId,token'
+  },
+  affirmPaymentsSaveOrder: {
+      mandatory: 'store,quoteId,sessionId,paymentData'
   }
 };
 
